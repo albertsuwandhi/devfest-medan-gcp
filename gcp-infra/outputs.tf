3767946fde63ea4b3f043ab0efe3e63a00c84e57
Albert Suwandhi <albert.suwandhi@gmail.com>
2023-12-09 22:47:45 +0700

    Remove All Resources

--- gcp-infra/outputs.tf
+++ gcp-infra/outputs.tf
@@ -1,7 +1,7 @@
-output "public_ip_vm_01" {
-  value       = google_compute_instance.vm-01.network_interface[0].access_config[0].nat_ip
-  description = "vm-01 Public IP"
-}
+# output "public_ip_vm_01" {
+#   value       = google_compute_instance.vm-01.network_interface[0].access_config[0].nat_ip
+#   description = "vm-01 Public IP"
+# }
 
 # output "public_ip_vm_02" {
 #   value       = google_compute_instance.vm-02.network_interface[0].access_config[0].nat_ip
